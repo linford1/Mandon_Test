b65e75ecc5bdbbfad0661087d47e84396769ce7d
linford1 <34671410+linford1@users.noreply.github.com>
2023-01-31 21:28:20 +0000

    Add project files.

---
+++ wwwroot/lib/jquery/Quoteform.js
@@ -0,0 +1,46 @@
+﻿<script type = "text/javascript">
+
+    $( document ).ready(function() {
+        $('#submitQuoteFormBtn').on('click', function (e) {
+            e.preventDefault(); //prevent the form from being submitted
+
+            $(this).prop('disabled', true);
+             
+            var form = $('#createQuoteForm');
+            $.ajax({
+                url: form.get(0).action,
+                type: form.get(0).method,
+                data: form.serialize(),
+                success: function (result) {
+                    $('#submitQuoteFormBtn').removeAttr('disabled');  
+                }
+            });
+        });
+
+        $(document).ajaxSuccess(function( event, xhr, settings ) {
+            var splitResponse = xhr.responseText.split(",")
+            var OS = splitResponse[2].split(":") 
+
+            console.log("User Agent: " +OS[1] + ", " + splitResponse[3].replace("}",""));
+
+            alert("We have recieved your form")
+            document.getElementById("createQuoteForm").reset();
+        });
+
+        $("#FlatRoofCheckBox").change(function () {
+            if (this.checked) {
+                $("#FlatRoofSize").attr("hidden", false)
+            }
+            else {
+                $("#FlatRoofSize").val("")
+                $("#FlatRoofSize").attr("hidden","hidden")
+            }
+        });
+    });
+  </script>
+
+<style>
+    Button#submitQuoteFormBtn:hover {
+        background-color: #4c9ed9;
+    }
+</style>
